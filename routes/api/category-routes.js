e9215936c399bd7bc2584398dac3f6dfe6509d2d
eljayman <lelandoj@gmail.com>
2022-12-21 00:30:14 -0700

    completed category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,16 +23,37 @@ router.get("/:id", async (req, res) => {
   return res.json(category);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  await Category.create({
+    category_name: req.body.category_name,
+  });
+  res.end();
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  res.end();
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.end();
 });
 
 module.exports = router;
